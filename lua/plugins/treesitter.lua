d387b47eac830a01e19242129ecae5fa70947b75
Ramsey Ramos <ramsey.ramos@gmail.com>
2024-02-29 08:23:50 +0800

    chores: various updates
    - changed ascii art for alpha
    - fix coding lua coding standards
    - added README.md
    - added .editorconfig

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript", "php", "phpdoc", "markdown" },
-			highlight = { enable = true },
-			inded = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "lua", "javascript", "php", "phpdoc", "markdown" },
+      highlight = { enable = true },
+      inded = { enable = true },
+    })
+  end,
 }
