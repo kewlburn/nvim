06fe8959a6fedef3b0a1e4241a9d485883fb01d6
Ramsey Ramos <ramsey.ramos@gmail.com>
2024-03-01 16:51:26 +0800

    feat: add auto-install in treesitter for highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			ensure_installed = { "lua", "javascript", "php", "phpdoc", "markdown" },
+			auto_install = true,
 			highlight = { enable = true },
 			inded = { enable = true },
 		})
