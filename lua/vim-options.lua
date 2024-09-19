50edc95ddbed525ea099f9fab0d2c305e38c59f3
Ramsey Ramos <ramsey.ramos@gmail.com>
2024-09-19 16:36:46 +0800

    updated formatter and linter

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,15 +11,20 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Disable Highlight Search" })
-vim.keymap.set("n", "<leader>fn", ":ene<CR>", { desc = "New File" })
-vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { desc = "LazyGit" })
-vim.keymap.set("n", "<leader>w", ":bd<CR>", { desc = "Close current buffer" })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>",
+               {desc = "Disable Highlight Search"})
+vim.keymap.set("n", "<leader>fn", ":ene<CR>", {desc = "New File"})
+vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", {desc = "LazyGit"})
+vim.keymap.set("n", "<leader>w", ":bd<CR>", {desc = "Close current buffer"})
 
-vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent", noremap = true, silent = true })
-vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Unindent", noremap = true, silent = true })
-vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent", noremap = true, silent = true })
-vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent", noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ">>",
+               {desc = "Indent", noremap = true, silent = true})
+vim.keymap.set("n", "<S-Tab>", "<<",
+               {desc = "Unindent", noremap = true, silent = true})
+vim.keymap.set("v", "<Tab>", ">gv",
+               {desc = "Indent", noremap = true, silent = true})
+vim.keymap.set("v", "<S-Tab>", "<gv",
+               {desc = "Unindent", noremap = true, silent = true})
 
 vim.opt.clipboard = "unnamedplus"
 vim.wo.number = true
